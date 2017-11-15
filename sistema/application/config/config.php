b23c81c0bb168a83ab3d7510823a758b0bd03399
budaboydark <blueinforbs@gmail.com>
2017-11-15 11:55:04 -0200

    Atualização de rotas com dominio.

--- sistema/application/config/config.php
+++ sistema/application/config/config.php
@@ -17,7 +17,7 @@ if (!defined('BASEPATH'))
   | path to your installation.
   |
  */
-$config['base_url'] = 'http://localhost/deliciasnocopinho/sistema';
+$config['base_url'] = 'http://admin.deliciasnocopinho.com/';
 
 /*
   |--------------------------------------------------------------------------
